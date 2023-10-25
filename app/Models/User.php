92b070764e312e231eaba353262d3ed7051aceae
Boiko Ihor <ihor.boiko@appflame.com>
2023-10-25 12:46:43 +0300

    user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,45 +1,55 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App\Models
+ *
+ * @property int id
+ * @property string created_at
+ * @property string login
+ * @property string password
+ * @property string remember_token
+ * @property string updated_at
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
+     * @var string[] $casts
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+    protected $casts = [
+        'password' => 'hashed',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
+     * @var string $dateFormat
+     */
+    protected $dateFormat = 'U';
+
+    /**
+     * @var string[] $fillable
+     */
+    protected $fillable = [
+        'login',
+        'password',
+        'role',
+    ];
+
+    /**
+     * @var string[] $hidden
      */
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,31 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     /**
-     * Run the migrations.
+     * @return void
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        Schema::create(
+            app(User::class)->getTable(),
+            function (Blueprint $table) {
+                $table->id();
+                $table->unsignedInteger('created_at');
+                $table->string('login')->unique();
+                $table->string('password');
+                $table->rememberToken();
+                $table->unsignedInteger('updated_at');
+            }
+        );
+
+        User::create([
+            'login' => 'igosja',
+            'password' => Hash::make('gfhjkm'),
+        ]);
     }
 
     /**
-     * Reverse the migrations.
+     * @return void
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists(app(User::class)->getTable());
     }
 };
