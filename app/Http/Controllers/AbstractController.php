85fa539c621d65dfee292f0e43fbe3e28042420a
Boiko Ihor <ihor.boiko@appflame.com>
2023-10-25 16:22:06 +0300

    login api

---
+++ app/Http/Controllers/AbstractController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller;
+
+/**
+ * Class AbstractController
+ * @package App\Http\Controllers
+ */
+abstract class AbstractController extends Controller
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
