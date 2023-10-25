0d22249b223408bd0d9698ded6c0a33d574d4fee
Boiko Ihor <ihor.boiko@appflame.com>
2023-10-25 16:31:34 +0300

    react

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{react}', function () {
+    return view('index');
+})->where('react', '^((?!api).)*$');
